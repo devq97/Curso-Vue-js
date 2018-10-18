be88c24f8b23db315f7d0b5a0b6bb79cfeaabf74
David Vargas <devq97@gmail.com>
2018-10-18 18:23:28 -0500

    Propiedades computadas - Quinto video

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const vm = new Vue({
   el: 'main',
   data: {
+    mensaje: "Hola mundo :)!",
     nuevaTarea: null,
     tareas: [
-      'Aprender Vue.js',
-      'Aprender ES6',
-      'Publicar algo todos los días'
+      {
+        titulo: 'Aprender Vue.js',
+        prioridad: true,
+        antiguedad: 23
+      },
+      {
+        titulo: 'Aprender ES6',
+        prioridad: false,
+        antiguedad: 135
+      },
+      {
+        titulo: 'Publicar algo todos los días',
+        prioridad: true,
+        antiguedad: 378
+      },
     ]
   },
   methods: {
@@ -13,7 +26,22 @@ const vm = new Vue({
       this.tareas.unshift(this.nuevaTarea);
       this.nuevaTarea = null;
     }
+  },
+
+  computed: {
+    mensajeAlReves() {
+      return this.mensaje.split('').reverse().join('')
+    },
+
+    tareasConPrioridad() {
+      return this.tareas.filter((tarea) => tarea.prioridad);
+    },
+
+    tareasPorAntiguedad() {
+      return this.tareas.sort((a, b) => b.antiguedad - a.antiguedad);
+    }
   }
+
 });
 
 
