6f536e0f74ecdfedfe8261c242b9441569332498
David Vargas <devq97@gmail.com>
2018-10-18 17:59:15 -0500

    Eventos - Cuarto video

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const vm = new Vue({
   el: 'main',
   data: {
-    laborales: ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes'],
+    nuevaTarea: null,
     tareas: [
-      {nombre: 'Hacer la compra', prioridad: 'baja'},
-      {nombre: 'Aprender Vue y Firebase', prioridad: 'alta'},
-      {nombre: 'Ir al gimnasio', prioridad: 'alta'},
-    ],
-
-    persona: {
-      nombre: 'David',
-      profesion: 'dev',
-      ciudad: 'Bogota'
-    }
+      'Aprender Vue.js',
+      'Aprender ES6',
+      'Publicar algo todos los días'
+    ]
   },
+  methods: {
+    agregarTarea() {
+      this.tareas.unshift(this.nuevaTarea);
+      this.nuevaTarea = null;
+    }
+  }
 });
+
+
+//Vainilla JavaScript
+
+// function agregarTarea() {
+//   const input = document.querySelector('input[type=text]');
+//   vm.tareas.unshift(input.value);
+//   input.value = '';
+// }
