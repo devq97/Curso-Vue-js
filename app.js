b1887dea0ca2c00ad72c57be2caeded1647857c5
David Vargas <devq97@gmail.com>
2018-10-19 12:08:32 -0500

    Instancia Vue - Septimo video

--- app.js
+++ app.js
@@ -1,44 +1,36 @@
-new Vue({
+const vm1 = new Vue({
   el: 'main',
   data: {
-    busqueda: '',
-    minimo: 5,
-    juegos: [
-      {
-        titulo: 'Battlefield 1',
-        genero: "FPS",
-        puntuacion: 9
-      },
-      {
-        titulo: 'Civilization VI',
-        genero: 'Estrategia',
-        puntuacion: 10
-      },
-      {
-        titulo: 'Resident Evil 7',
-        genero: 'Survival Horror',
-        puntuacion: 7
-      },
-    ]
+    mensaje: 'Instancia Vue 1',
+  },
+
+  beforeUpdate() {
+    console.log('beforeUpdate: ', this.mensaje);
+  },
+
+  updated() {
+    console.log('Update: ', this.mensaje);
+  },
+
+  methods: {
+
+    alReves() {
+      this.mensaje = this.mensaje.split('').reverse().join('');
+    }
+
   },
 
   computed: {
-    mejoresJuegos() {
-      return this.juegos.filter((juego) => juego.puntuacion >= this.minimo);
-    },
-
-    buscarJuego() {
-      return this.juegos.filter((juego) => juego.titulo.includes(this.busqueda));
+    mensajeMayusculas() {
+      return this.mensaje.toUpperCase();
     }
   }
 
 });
 
-
-//Vainilla JavaScript
-
-// function agregarTarea() {
-//   const input = document.querySelector('input[type=text]');
-//   vm.tareas.unshift(input.value);
-//   input.value = '';
-// }
+const vm2 = new Vue({
+  el: '#app',
+  data: {
+    mensaje: 'Instancia Vue 2',
+  }
+});
